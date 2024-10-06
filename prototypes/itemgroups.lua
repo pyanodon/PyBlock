25e8ae79d7d6120d557645729469df3e491051e2
protocol_1903 <fisher.braydon@yahoo.com>
2024-10-06 13:52:43 -0700

    itemgroups updated again

--- prototypes/itemgroups.lua
+++ prototypes/itemgroups.lua
@@ -20,9 +20,21 @@ data:extend(
     },
     {
       type = "item-subgroup",
+      name = "py-cp-buildings-mk01",
+      group = "coal-processing",
+      order = "a-a-b"
+    },
+    {
+      type = "item-subgroup",
       name = "py-fusion-buildings-mk00",
       group = "fusion-energy",
       order = "a-a-a"
+    },
+    {
+      type = "item-subgroup",
+      name = "py-fusion-buildings-mk01",
+      group = "fusion-energy",
+      order = "a-a-b"
     },
   }
 )
