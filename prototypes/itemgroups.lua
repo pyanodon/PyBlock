d47ccaab60c887ff4c08bb21a4684210ad077d20
protocol_1903 <fisher.braydon@yahoo.com>
2024-10-06 13:33:40 -0700

    itemgroups for mk00 buildings

--- prototypes/itemgroups.lua
+++ prototypes/itemgroups.lua
@@ -1,17 +1,28 @@
 data:extend(
+  {
     {
-        {
-            type = "item-subgroup",
-            name = "py-alienlife-buildings-mk00",
-            group = "py-alienlife",
-            order = "a-a-a"
-        },
-        {
-            type = "item-subgroup",
-            name = "py-alienlife-buildings-mk01",
-            group = "py-alienlife",
-            order = "a-a-b"
-        },
-
-    }
+      type = "item-subgroup",
+      name = "py-alienlife-buildings-mk00",
+      group = "py-alienlife",
+      order = "a-a-a"
+    },
+    {
+      type = "item-subgroup",
+      name = "py-alienlife-buildings-mk01",
+      group = "py-alienlife",
+      order = "a-a-b"
+    },
+    {
+      type = "item-subgroup",
+      name = "py-cp-buildings-mk00",
+      group = "coal-processing",
+      order = "a-a-a"
+    },
+    {
+      type = "item-subgroup",
+      name = "py-fusion-buildings-mk00",
+      group = "fusion-energy",
+      order = "a-a-a"
+    },
+  }
 )
