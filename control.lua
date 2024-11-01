dea6387dff24f50c9cdb6777612caf05cbd3bb1a
protocol_1903 <fisher.braydon@yahoo.com>
2024-11-01 16:40:12 -0700

    increase starting logs

--- control.lua
+++ control.lua
@@ -4,7 +4,7 @@ if not script.active_mods['pylandblock'] then
             local created_items = remote.call('freeplay', 'get_created_items')
             created_items['landfill'] = 1000
             created_items['stone'] = 250
-            created_items['log'] = 100
+            created_items['log'] = 200
             created_items["iron-plate"] = 1000
             created_items["copper-plate"] = 500
             created_items["transport-belt"] = 100
