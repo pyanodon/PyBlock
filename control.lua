9e31b0647a1de8cc056289e3c2cf58a5fee82ce8
protocol_1903 <fisher.braydon@yahoo.com>
2024-11-25 16:06:02 -0700

    fixed terrain gen, finally

--- control.lua
+++ control.lua
@@ -1,22 +1,31 @@
-if not script.active_mods['pylandblock'] then
-    script.on_init(function(event)
-        if remote.interfaces['freeplay'] then
-            local created_items = remote.call('freeplay', 'get_created_items')
-            created_items['landfill'] = 1000
-            created_items['stone'] = 250
-            created_items['log'] = 200
-            created_items["iron-plate"] = 1000
-            created_items["copper-plate"] = 500
-            created_items["transport-belt"] = 100
-            created_items["burner-inserter"] = 50
-            created_items['py-tank-1000'] = 1
-            created_items['py-tank-3000'] = 1
-            created_items['py-tank-5000'] = 1
-            created_items['py-tank-8000'] = 1
-            created_items["stone-furnace"] = 1
-            created_items["py-sinkhole"] = 2
-            created_items["py-gas-vent"] = 2
-            remote.call('freeplay', 'set_created_items', created_items)
-        end
-    end)
-end
+script.on_init(function(event)
+  if remote.interfaces['freeplay'] then
+    local created_items = remote.call('freeplay', 'get_created_items')
+    created_items['landfill'] = 1000
+    created_items['stone'] = 250
+    created_items['log'] = 200
+    created_items["iron-plate"] = 1000
+    created_items["copper-plate"] = 500
+    created_items["transport-belt"] = 100
+    created_items["burner-inserter"] = 50
+    created_items['py-tank-1000'] = 1
+    created_items['py-tank-3000'] = 1
+    created_items['py-tank-5000'] = 1
+    created_items['py-tank-8000'] = 1
+    created_items["stone-furnace"] = 1
+    created_items["py-sinkhole"] = 2
+    created_items["py-gas-vent"] = 2
+    remote.call('freeplay', 'set_created_items', created_items)
+  end
+end)
+
+script.on_event(defines.events.on_player_created, function(event)
+  local player = game.players[event.player_index]
+  if not player.valid then return end
+  local nauvis = game.surfaces["nauvis"]
+  if not nauvis then return end
+  local elevation = game.surfaces["nauvis"].map_gen_settings.property_expression_names.elevation
+	if elevation ~= "pyblock_classic" and elevation ~= "pyblock_island" and elevation ~= "pyblock_archipeligo" then
+		player.print {"messages.pyblock-warning-no-preset"}
+  end
+end)
--- prototypes/tiles/tiles.lua
+++ prototypes/tiles/tiles.lua
@@ -1,11 +1,11 @@
-for _, t in pairs(data.raw.tile) do
-    t.autoplace = { probability_expression = "0" }
-end
+-- for _, t in pairs(data.raw.tile) do
+--     t.autoplace = { probability_expression = "0" }
+-- end
 
-data.raw.planet.nauvis.map_gen_settings.autoplace_settings.tile.settings.landfill = {}
+-- data.raw.planet.nauvis.map_gen_settings.autoplace_settings.tile.settings.landfill = {}
 
-data.raw.tile["landfill"].autoplace = { probability_expression = "if(elevation > -2, 1, -inf)" }
+-- data.raw.tile["landfill"].autoplace = { probability_expression = "if(elevation > 0, 1, -inf)" }
 
-data.raw.tile["water"].autoplace = { probability_expression = "water_base(3, 0.1)" }
+-- data.raw.tile["water"].autoplace = { probability_expression = "if(elevation > -3, 0.1, -inf)" }
 
-data.raw.tile["deepwater"].autoplace = { probability_expression = "water_base(0, 0.01)" }
+-- data.raw.tile["deepwater"].autoplace = { probability_expression = "0.001" }
