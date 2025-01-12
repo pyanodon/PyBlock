c12aa48c969f8b257458a45084af9be4baa0a3ea
protocol_1903 <fisher.braydon@yahoo.com>
2025-01-12 00:34:07 -0700

    Buffed the basic fish breeding recipe from 270s -> 200s

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -142,7 +142,7 @@ local breed_fish = table.deepcopy(data.raw["recipe"]["breed-fish-1"])
 breed_fish.name = "breed-fish-simple"
 data.raw.recipe["breed-fish-simple"] = breed_fish
 
-RECIPE("breed-fish-simple"):remove_ingredient("small-lamp"):add_unlock("fish-mk01"):set_fields{energy_required = 270, results = {{type = "item", name = "fish", amount = 12}, {type = "fluid", name = "waste-water", amount = 100}}}
+RECIPE("breed-fish-simple"):remove_ingredient("small-lamp"):add_unlock("fish-mk01"):set_fields{energy_required = 200, results = {{type = "item", name = "fish", amount = 12}, {type = "fluid", name = "waste-water", amount = 100}}}
 
 TECHNOLOGY("water-animals-mk01"):remove_pack("py-science-pack-1"):set_fields{prerequisites = {}}
 
