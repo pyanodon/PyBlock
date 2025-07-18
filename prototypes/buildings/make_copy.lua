daa34291e40a6c438114aecc7d8beef6ff36c2af
protocol-1903 <fisher.braydon@yahoo.com>
2025-07-17 21:39:54 -0700

    fix the function

--- prototypes/buildings/make_copy.lua
+++ prototypes/buildings/make_copy.lua
@@ -2,10 +2,15 @@ return function (entity, new_name)
   new_entity = table.deepcopy(data.raw["assembling-machine"][entity])
   new_entity.name = new_name
   new_entity.localised_name = nil
-  for _, sprite in pairs(new_entity.animation.layers) do
-    if sprite.tint then sprite.tint = {r = 0.5, g = 0.5, b = 0.5, a = 1.0} end
+  for _, layers in pairs{
+    new_entity.graphics_set.animation and new_entity.graphics_set.animation.layers or {},
+    new_entity.graphics_set.working_visualisations or {},
+  } do
+    for _, sprite in pairs(layers or {}) do
+      if sprite.tint then sprite.tint = {r = 0.5, g = 0.5, b = 0.5, a = 1.0} end
+    end
   end
-  new_entity.mineable.result = new_name
+  new_entity.minable.result = new_name
   new_entity.crafting_speed = new_entity.crafting_speed * 0.5
   data.raw["assembling-machine"][new_name] = new_entity
   return ENTITY(new_name)
