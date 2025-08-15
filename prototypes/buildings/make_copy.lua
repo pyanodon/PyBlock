d07b0f9c4747af813c04d25a5aa12743888642fe
lambdalemon <lemonjameslem@protonmail.com>
2025-08-15 07:10:35 -0400

    Fixed them giving the wrong item back

--- prototypes/buildings/make_copy.lua
+++ prototypes/buildings/make_copy.lua
@@ -1,23 +1,27 @@
 return function (base_name)
-  new_entity = table.deepcopy(data.raw["assembling-machine"][base_name .. "-mk01"] or data.raw.furnace[base_name .. "-mk01"])
+  local entity_name = base_name .. "-mk01"
+  local new_entity = table.deepcopy(data.raw["assembling-machine"][entity_name] or data.raw.furnace[entity_name])
   new_entity.name = base_name .. "-mk00"
-  new_entity.localised_name = {"?", settings.startup["disable-pyblock-fun-names"].value and {"entity-name-alt." .. base_name .. "-mk00"} or {"entity-name." .. base_name .. "-mk00"}, {"item-tier.mk00", {"entity-name." .. base_name}}}
-  new_entity.localised_description = {"?", {"entity-description." .. base_name .. "-mk00"}, {"entity-description." .. base_name}}
+  ENTITY(new_entity):set_fields{
+    icon = "__PyBlock__/graphics/icons/" .. base_name .. "-mk00.png",
+    minable = {
+      result = base_name .. "-mk00",
+      mining_time = ENTITY(entity_name).minable.mining_time
+    },
+    localised_name = {"?", settings.startup["disable-pyblock-fun-names"].value and {"entity-name-alt." .. base_name .. "-mk00"} or {"entity-name." .. base_name .. "-mk00"}, {"item-tier.mk00", {"entity-name." .. base_name}}},
+    localised_description = {"?", {"entity-description." .. base_name .. "-mk00"}, {"entity-description." .. base_name}},
+    crafting_speed = ENTITY(entity_name).crafting_speed * 0.5,
+    fast_replaceable_group = base_name,
+    module_slots = 0,
+    next_upgrade = base_name .. "-mk01",
+  }
   for _, layers in pairs{
-    new_entity.graphics_set.animation and new_entity.graphics_set.animation.layers or {},
-    new_entity.graphics_set.working_visualisations or {},
+    ENTITY(base_name .. "-mk00").graphics_set.animation and new_entity.graphics_set.animation.layers or {},
+    ENTITY(base_name .. "-mk00").graphics_set.working_visualisations or {},
   } do
     for _, sprite in pairs(layers or {}) do
       if sprite.tint then sprite.tint = {r = 0.5, g = 0.5, b = 0.5, a = 1.0} end
     end
   end
-  new_entity.icon = "__PyBlock__/graphics/icons/" .. base_name .. "-mk00.png"
-  new_entity.minable.result = base_name .. "-mk00"
-  new_entity.crafting_speed = new_entity.crafting_speed * 0.5
-  new_entity.fast_replaceable_group = base_name
-  new_entity.module_slots = 0
-  new_entity.crafting_speed = 0.5
-  new_entity.next_upgrade = base_name .. "-mk01"
-  data.raw[new_entity.type][base_name .. "-mk00"] = new_entity
   return ENTITY(base_name .. "-mk00")
 end
