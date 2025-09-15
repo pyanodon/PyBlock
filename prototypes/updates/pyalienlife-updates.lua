407060c2c0a6b245b7396efcdb3c1d7cc2317ae6
protocol-1903 <fisher.braydon@yahoo.com>
2025-09-14 21:48:34 -0700

    make uranium science require bhoddos as to not trap the player

--- prototypes/updates/pyalienlife-updates.lua
+++ prototypes/updates/pyalienlife-updates.lua
@@ -237,6 +237,7 @@ RECIPE("cadaveric-pb"):remove_unlock("phytomining"):add_unlock("phytomining-mk02
 RECIPE("s-biomass-extraction"):remove_unlock("phytomining"):add_unlock("phytomining-mk02")
 
 -- Bhoddos to uranium
+TECHNOLOGY("chemical-science-pack"):add_prereq("bhoddos")
 RECIPE("ur-biomass-extraction"):replace_result("uranium-ore", "uranium-ore", 50):replace_ingredient("ur-biomass", "ur-biomass", 4):set_fields {energy_required = 80}:remove_unlock("phytomining-mk03"):add_unlock("phytomining-mk02")
 RECIPE("bhodos-ur"):set_fields {
   ingredients = {
