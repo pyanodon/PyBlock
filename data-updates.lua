df88fb0b147b35be5c6a6244c41b2bcd37aa8b3e
protocol-1903 <67478786+protocol-1903@users.noreply.github.com>
2025-10-24 11:28:45 -0700

    fix nickel being an ingredient instead of a product in basic soot separation

--- data-updates.lua
+++ data-updates.lua
@@ -77,7 +77,7 @@ end
 
 TECHNOLOGY("excavation-1"):remove_pack("logistic-science-pack"):remove_pack("chemical-science-pack")
 
-RECIPE("soot-separation"):add_ingredient{type = "item", name = "ore-nickel", amount = 1, probability = 0.1}
+RECIPE("soot-separation"):add_result{type = "item", name = "ore-nickel", amount = 1, probability = 0.1}
 
 RECIPE("mining-antimony"):replace_unlock("excavation-2", "excavation-1")
 
