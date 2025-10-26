cfc19de12c3bb7e0ed2e5a926526797158b6f437
protocol-1903 <fisher.braydon@yahoo.com>
2025-10-25 19:38:39 -0700

    Fixed Automated Screener Mk 00 not being an ingredient for the Mk 01 version

--- data-updates.lua
+++ data-updates.lua
@@ -209,6 +209,7 @@ RECIPE("slaughterhouse-mk01"):add_ingredient{type = "item", name = "slaughterhou
 RECIPE("soil-extractor-mk01"):replace_unlock("automation-science-pack", "soil-washing"):replace_ingredient("burner-mining-drill", "soil-extractor-mk00", 1)
 RECIPE("solid-separator"):replace_unlock("ash-separation", "steel-processing"):add_ingredient{type = "item", name = "solid-separator-mk00", amount = 1}:add_ingredient_amount("small-parts-01", -20):add_ingredient_amount("steel-plate", -10):add_ingredient_amount("inductor1", -5)
 -- RECIPE("washer"):replace_ingredient("steam-engine", "washer-mk00", 1):replace_unlock("soil-washing", "electrolysis")
+RECIPE("automated-screener-mk01"):replace_ingredient("fluid-drill-mk01", "automated-screener-mk00", 1)
 RECIPE("wpu-mk01"):replace_unlock("automation-science-pack", "wood-processing"):add_ingredient{type = "item", name = "inductor1", amount = 12} :add_ingredient{type = "item", name = "wpu-mk00", amount = 1}.enabled = false
 
 -- move check valve from fluid handling to assembly
